818dba7a43b1de4bc45a22340ad73333149c7046
martin <martin@6d9cadbc-6908-e40d-0dd0-ce57b7db7bff>
2018-07-18 20:23:40 +0000

    add extension
    git-svn-id: https://github.com/Martas86/TamperMonkey/trunk@11 6d9cadbc-6908-e40d-0dd0-ce57b7db7bff

---
+++ Release/Livesport-Highlighter.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Livesport Highlighter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      https://www.livesport.cz/*
+// @exclude      https://www.livesport.cz/zapas/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    $(document).ready(function() {
+        //alert($("#mc").attr("class"));
+        $("#mc").on('hide', '#fs_overlay', function() {
+            //alert('#foo is hidden');
+            highlight();
+        });
+        $("#mc").on('click', '.tournament-pages-link', function() {
+            //alert('#foo is hidden');
+            highlight();
+        });
+
+        //$("span.flag fl_62").closest('tr').css('color','blue');
+    });
+
+    function highlight() {
+        setTimeout(function(){
+            $("span.flag.fl_62, span.padl:contains('(Cze)')").closest('tr').css('color','crimson');
+        }, 500);
+    }
+
+    (function ($) {
+        $.each(['show', 'hide'], function (i, ev) {
+            var el = $.fn[ev];
+            $.fn[ev] = function () {
+                this.trigger(ev);
+                return el.apply(this, arguments);
+            };
+        });
+    })(jQuery);
+})();
