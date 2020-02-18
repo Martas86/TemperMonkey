4d6c3563465b68c4b1ba6387b4abe07b9121d8ff
ŠTÍBAL Martin <sti@coaut.cz>
2020-02-18 13:02:21 +0100

    a

--- Release/CSFD-Ranks.user.js
+++ Release/CSFD-Ranks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSFD - Ranks
 // @namespace    https://github.com/Martas86/TamperMonkey
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.csfd.cz/zebricky/specificky-vyber/*
