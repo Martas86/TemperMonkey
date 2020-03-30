f43a7fb45e6eb7bf4715839e9f78c9b94162a7f4
Martin <m.stibal@gmail.com>
2020-03-30 19:46:26 +0200

    Link

--- Release/CSFD-TV.user.js
+++ Release/CSFD-TV.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         CSFD - TV
 // @namespace    https://github.com/Martas86/TamperMonkey
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.csfd.cz/televize/
+// @match        https://www.csfd.cz/televize/*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/Martas86/TemperMonkey/master/Release/CSFD-TV.user.js
 // @updateURL    https://raw.githubusercontent.com/Martas86/TemperMonkey/master/Release/CSFD-TV.user.js
